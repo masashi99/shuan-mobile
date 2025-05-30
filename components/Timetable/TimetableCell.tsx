487ac30831e4402155644ac258125fb0fcc4838b
masashi99 <sgmdq601@gmail.com>
2025-05-30 15:46:44 +0900

    feat: モーダル作成

--- components/Timetable/TimetableCell.tsx
+++ components/Timetable/TimetableCell.tsx
@@ -1,12 +1,59 @@
-import { Pressable, View } from "react-native";
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
+import Modal from "react-native-modal";
 
 export function TimetableCell() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handlePress = async () => setModalVisible(true);
+
+  const handleRequestClose = () => setModalVisible(false);
+
+  const handleOk = () => {
+    console.log("true");
+    setModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    console.log("false");
+    setModalVisible(false);
+  };
+
   return (
     <View style={{ flex: 1 }}>
       <Pressable
-        onPress={() => console.log("press")}
+        onPress={handlePress}
         style={({ pressed }) => [{ width: "100%", height: "100%" }, pressed && { backgroundColor: "red" }]}
-      ></Pressable>
+      />
+      <Modal
+        isVisible={modalVisible}
+        swipeDirection="down"
+        onSwipeComplete={handleRequestClose}
+        style={{ justifyContent: "flex-end", margin: 0 }}
+      >
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "row",
+            gap: 16,
+            backgroundColor: "white",
+            borderRadius: 16,
+            height: "80%"
+          }}
+        >
+          <Pressable onPress={handleOk}>
+            <View>
+              <Text>Ok</Text>
+            </View>
+          </Pressable>
+          <Pressable onPress={handleCancel}>
+            <View>
+              <Text>Cancel</Text>
+            </View>
+          </Pressable>
+        </View>
+      </Modal>
     </View>
   );
 }
