02be878c1fd22f904574868e411978ff7cd79af1
masashi99 <sgmdq601@gmail.com>
2025-05-30 12:00:35 +0900

    feat: ドロワーアイコン変更

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Drawer } from "expo-router/drawer";
+import { Calendar, CalendarCog } from "lucide-react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
@@ -9,14 +10,18 @@ export default function RootLayout() {
           name="index"
           options={{
             title: "週案",
-            headerTitle: "週案"
+            headerTitle: "週案",
+            drawerIcon: () => <Calendar />,
+            drawerActiveTintColor: "green"
           }}
         />
         <Drawer.Screen
           name="timetable"
           options={{
             title: "時間割設定",
-            headerTitle: "時間割設定"
+            headerTitle: "時間割設定",
+            drawerIcon: () => <CalendarCog />,
+            drawerActiveTintColor: "green"
           }}
         />
       </Drawer>
