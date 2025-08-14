af727bc5f4824c907e09459c11bf5ace2afc4b5c
masashi99 <sgmdq601@gmail.com>
2025-08-14 21:35:47 +0900

    remove: day of week table

--- db/relations/dayOfWeekRelations.ts
+++
@@ -1,7 +0,0 @@
-import { dayOfWeeks } from "../dayOfWeeks";
-import { timeScheduleSlots } from "../timeScheduleSlots";
-import { relations } from "drizzle-orm";
-
-export const dayOfWeekRelations = relations(dayOfWeeks, ({ many }) => ({
-  timeScheduleSlots: many(timeScheduleSlots)
-}));
