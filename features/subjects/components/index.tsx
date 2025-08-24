9e89139acccba46b27d53b005326d6e12d121e00
masashi99 <sgmdq601@gmail.com>
2025-08-24 20:55:57 +0900

    remove: tmp code

--- features/subjects/components/index.tsx
+++ features/subjects/components/index.tsx
@@ -1,28 +1,10 @@
 import { CurrentToast } from "../../shared/components/CurrentToast";
-import { useCreateSubject } from "../api/useCreateSubject";
 import { useGetSubjects } from "../api/useGetSubjects";
 import { SubjectsList } from "./SubjectsList";
-import { useToastController } from "@tamagui/toast";
 import { View } from "react-native";
 
 export function Subjects() {
   const { data: subjects } = useGetSubjects();
-  const { mutate: createSubject } = useCreateSubject();
-  const toast = useToastController();
-
-  const handlePress = () => {
-    // TODO: 検証ようなので修正する
-    const randomId = Math.random().toString(36).substring(2, 15);
-    createSubject({
-      name: "Subject" + randomId,
-      id: randomId,
-      totalRequiredLessons: 0,
-      courses: []
-    });
-    toast.show("Successfully saved!", {
-      message: "Don't worry, we've got your data."
-    });
-  };
 
   return (
     <View style={{ flex: 1 }}>
