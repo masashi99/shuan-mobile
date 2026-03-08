36b674587a0dcc6b5ae6bb2be8ea129dc71bd737
masashi99 <sgmdq601@gmail.com>
2026-03-08 20:55:50 +0900

    layout戻し

--- app/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Tabs } from "expo-router";
-
-export default function RootLayout() {
-  return <Tabs>
-    <Tabs.Screen name="schedule" options={{title: "スケジュール"}} />
-    <Tabs.Screen name="timetable" options={{title: "時間割"}} />
-    <Tabs.Screen name="unit" options={{title: "単元"}} />
-  </Tabs>;
-}
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,9 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Tabs>
+    <Tabs.Screen name="schedule" options={{title: "スケジュール"}} />
+    <Tabs.Screen name="timetable" options={{title: "時間割"}} />
+    <Tabs.Screen name="unit" options={{title: "単元"}} />
+  </Tabs>;
 }
