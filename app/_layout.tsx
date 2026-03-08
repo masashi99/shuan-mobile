fdc88ccc82f0f93cf2acb1d914361831a86f0993
masashi99 <sgmdq601@gmail.com>
2026-03-08 20:48:37 +0900

    初期化のテンプレート変更

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,9 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  return <Tabs>
+    <Tabs.Screen name="schedule" options={{title: "スケジュール"}} />
+    <Tabs.Screen name="timetable" options={{title: "時間割"}} />
+    <Tabs.Screen name="unit" options={{title: "単元"}} />
+  </Tabs>;
 }
---
+++ app/schedule.tsx
@@ -0,0 +1,5 @@
+import { Text, View } from "react-native";
+
+export default function Schedule() {
+  return <View><Text>schedule</Text></View>
+}
